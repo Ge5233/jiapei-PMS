adafbdd579d451657121d1aa69613b3ff923ec69
Ge5233 <ge5233@github.com>
2026-08-13 20:58:55 +0800

    fix BOM: single x-for for sub-items (Alpine nested template bug)

--- self_product_edit.php
+++ self_product_edit.php
@@ -435,10 +435,9 @@ require __DIR__ . '/includes/views/header.php';
                                             </div>
                                         </td>
                                     </tr>
-                                    <!-- 配件行 -->
-                                    <template x-if="(it.subs||[]).length>0">
-                                        <template x-for="(s, si) in (it.subs||[])" :key="si">
-                                            <tr x-show="!it._collapsed" class="border-b border-slate-100 align-top text-xs">
+                                    <!-- 配件行（单层 x-for，空数组自然不渲染） -->
+                                    <template x-for="(s, si) in (it.subs||[])" :key="si">
+                                        <tr x-show="!it._collapsed" class="border-b border-slate-100 align-top text-xs">
                                                 <td class="py-1 pr-1 text-slate-400" x-text="ii+1+'.'+(si+1)"></td>
                                                 <td class="py-1 pr-1">
                                                     <select x-model="s.src" class="text-xs border rounded py-0.5 w-full" @change="srcChanged(s)">
